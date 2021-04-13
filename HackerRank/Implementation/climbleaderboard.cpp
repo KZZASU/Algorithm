81788c9dbf9bc349c0adb9d415164a81dee5b028
KZZASU <seongug2@gmail.com>
2021-04-13 21:26:21 +0900

    speed up 1

--- HackerRank/Implementation/climbleaderboard.cpp
+++ HackerRank/Implementation/climbleaderboard.cpp
@@ -3,39 +3,33 @@
 #include <algorithm>
 #include "climbleaderboard.h"
 
-bool myfunction(int i, int j) { return (i > j); }
 
+//for every integer in player, rank(dense) the integer with the value in the ranked vector
+bool myfunction(int i, int j) { return (i > j); }
 vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player)
 {
 	vector<int> result{};
 	vector<int>::size_type j{ 0 };
 	for (j; j < player.size(); j++)
 	{
-		vector<int> ranked_copy{ ranked };
-		ranked_copy.push_back(player[j]);
-		sort(ranked_copy.begin(), ranked_copy.end(), myfunction);
-
 		vector<int>::size_type index{ 0 };
 		vector<int> ranking{};
 		int rank{ 1 };
-		for (index; index < ranked_copy.size() - 1; index++)
+
+		while ((ranked[index] > player[j]) && (index < ranked.size() - 1))
 		{
-			ranking.push_back(rank);
-			if (ranked_copy[index] > ranked_copy[index + 1])
+			if (ranked[index] > ranked[index + 1])
 			{
 				rank++;
 			}
+			index++;
 		}
-		ranking.push_back(rank);
-
-		for (size_t k{ 0 }; k < ranked_copy.size(); k++)
+		if (ranked[index] > player[j])
 		{
-			if (ranked_copy[k] == player[j])
-			{
-				result.push_back(ranking[k]);
-				break;
-			}
+			rank++;
 		}
+
+		result.push_back(rank);
 	}
 	return result;
 }
